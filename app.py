104bac4a6cb3e2b6783b07abe837eff7eb7867bd
iron-dot <85775601+iron-dot@users.noreply.github.com>
2023-08-03 00:55:23 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,25 @@
 import torch
-from video_diffusion_pytorch import Unet3D, GaussianDiffusion
+from diffusers import DiffusionPipeline, DPMSolverMultistepScheduler
+from diffusers.utils import export_to_video
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+pipe = DiffusionPipeline.from_pretrained("damo-vilab/text-to-video-ms-1.7b", torch_dtype=torch.float16, variant="fp16")
+pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
+pipe.enable_model_cpu_offload()
 
-model = Unet3D(
-    dim = 64,
-    use_bert_text_cond = True,  # this must be set to True to auto-use the bert model dimensions
-    dim_mults = (1, 2, 4, 8),
-)
-
-diffusion = GaussianDiffusion(
-    model,
-    image_size = 32,    # height and width of frames
-    num_frames = 5,     # number of video frames
-    timesteps = 1000,   # number of steps
-    loss_type = 'l1'    # L1 or L2
-)
-
-videos = torch.randn(3, 3, 5, 32, 32) # video (batch, channels, frames, height, width)
-
-text = [
-    'a whale breaching from afar',
-    'young girl blowing out candles on her birthday cake',
-    'fireworks with blue and green sparkles'
-]
-
-loss = diffusion(videos, cond = text)
-loss.backward()
-# after a lot of training
-
-sampled_videos = diffusion.sample(cond = text, cond_scale = 2)
-sampled_videos.shape # (3, 3, 5, 32, 32)
+#prompt = "Spiderman is surfing"
+def video(prompt):
+    
+    video_frames = pipe(prompt, num_inference_steps=25).frames
+    video_path = export_to_video(video_frames)
+    return video_path
+import gradio as gr
+with gr.Blocks() as demo:
+        gr.Markdown("AGI AI assistant system(3차 베타테스트)")
+        with gr.Tab("video generate"):
+            
+            name_input = gr.Textbox()
+            name_output = gr.Video()
+            name_button = gr.Button("입력")
+            name_button.click(video, inputs=name_input, outputs=name_output)
+            
+demo.launch(debug=True, share=True)
